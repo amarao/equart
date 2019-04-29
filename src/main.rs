3d8c055044cff51936b11acf4dff67877d1e4ce4
George Shuklin <george.shuklin@gmail.com>
2019-04-30 00:19:49 +0300

    I almost win 1/sin(x), need to deal with None

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,11 @@ impl Point for Turtle {
     }
 }
 
-const DRAW_X: u32 = 1024;
-const FUNC_X: f64 = 4.0;
-const DRAW_Y: u32 = 1024;
-const FUNC_Y: f64 = 4.0;
+const DRAW_X: u32 = 1920;
+const DRAW_Y: u32 = 1080;
+
+const FUNC_X: f64 = 20.0;
+const FUNC_Y: f64 = 20.0;
 
 
 struct Pixel{x:f64, y:f64, dx:f64, dy:f64}
@@ -32,26 +33,44 @@ fn conv(i: i32, j:i32) -> Pixel {
     Pixel{x, y, dx, dy}
 }
 
+fn float_minmax<'a, T>(seq: T) -> (f64, f64) where
+    T: Iterator<Item=&'a f64> + Clone
+{
+    let min:f64 = *(seq.clone().min_by(|a:&&f64, b:&&f64| (**a).partial_cmp(*b).unwrap()).unwrap());
+    let max:f64 = *(seq.clone().max_by(|a:&&f64, b:&&f64| (**a).partial_cmp(*b).unwrap()).unwrap());
+    //let max:f64 = seq.max_by(|a:&f64, b:&f64| (*a).partial_cmp(b).unwrap()).unwrap();
+    // println!("{}, {}", min, max);
+    (min, max)
+}
+
+fn sign_change<'a, T>(points: T) -> bool where
+    T: Iterator<Item=&'a f64> + Clone
+{
+    let (min_val, max_val) = float_minmax(points);
+    // println!("{}, {}", min_val.signum(), max_val.signum());
+    min_val.signum() != max_val.signum()
+}
 
 fn within<F>(pixel:Pixel, eq: F) -> bool where
     F: Fn(f64, f64) -> f64
 {
-    let v1 = eq(pixel.x + pixel.dx/2.0, pixel.y + pixel.dy/2.0);
-    if v1.abs() < ((pixel.dx/2.0).abs()).min((pixel.dy/2.0).abs()){
-        return true
+    let x = pixel.x;
+    let y = pixel.y;
+    let dx = pixel.dx;
+    let dy = pixel.dy;
+    let v1 = eq(x + dx/2.0, y + dy/2.0);
+    if v1.abs() < ((dx/2.0).abs()).min((dy/2.0).abs()){
+        return true;
     }
-    // let v2 = eq(pixel.x+pixel.dx, pixel.y);
-    // let v3 = eq(pixel.x, pixel.y+pixel.dy);
-    // let v4 = eq(pixel.x + pixel.dx, pixel.y + pixel.dy);
-    // if [v1,v2,v3,v4].iter().map(abs).min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap() < &((pixel.dx).min(pixel.dy)) {
-    //     return true
-    // }
-    false
+    let corners: [f64;4] = [eq(x, y), eq(x+dx, y), eq(x, y+dy), eq(x+dx, y+dy)];
+
+    sign_change(corners.iter())
 }
 
 fn main() {
     let mut turtle = Turtle::new();
-    let eq = |x:f64, y:f64| x.sin() - y;
+    // let eq = |x:f64, y:f64| x.sin() - y;
+    let eq = |x:f64, y:f64| (1.0/x).sin() - y;
     turtle.drawing_mut().set_size((DRAW_X+50, DRAW_Y+50));
     turtle.set_speed("instant");
     for i in -(DRAW_X as i32)/2..(DRAW_X as i32)/2 {
