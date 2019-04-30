663ffaa65f17a31628a0f4c910392d96504b0dcb
George Shuklin <george.shuklin@gmail.com>
2019-04-30 13:33:12 +0300

    Better version, iterations over lattice

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ trait Point {
 
 impl Point for Turtle {
     fn point(&mut self, x:f64, y:f64){
+
         self.pen_up();
         self.go_to([x, y]);
         self.pen_down();
@@ -16,11 +17,12 @@ impl Point for Turtle {
     }
 }
 
+
 const DRAW_X: u32 = 1920;
 const DRAW_Y: u32 = 1080;
 
-const FUNC_X: f64 = 0.1;
-const FUNC_Y: f64 = 3.0;
+const FUNC_X: f64 = 0.0192;
+const FUNC_Y: f64 = 2.2;
 
 
 struct Pixel{x:f64, y:f64, dx:f64, dy:f64}
@@ -33,107 +35,44 @@ fn conv(i: i32, j:i32) -> Pixel {
     Pixel{x, y, dx, dy}
 }
 
-// fn float_minmax<'a, T>(seq: T) -> (f64, f64) where
-//     T: Iterator<Item=&'a f64> + Clone
-// {
-//     let mut min: Option<f64> = None;
-//     let mut max: Option<f64> = None;
-//     for &num in seq {
-//         min = match min {
-//             Some(val) if val > num => {Some(num)},
-//             Some(val) => {Some(val)},
-//             None => {Some(num)},
-//         };
-//         max = match max {
-//             Some(val) if val < num => {Some(num)},
-//             Some(val) => {Some(val)},
-//             None => {Some(num)},
-//         }
-//     }
-//     //let max:f64 = seq.max_by(|a:&f64, b:&f64| (*a).partial_cmp(b).unwrap()).unwrap();
-//     // println!("{}, {}", min, max);
-//     (min.unwrap(), max.unwrap())
-// }
-
-fn sign_change<'a, T>(points: T) -> bool where
-    T: Iterator<Item=&'a f64> + Clone
-{
-    let mut sign: Option<bool> = None;
-    for &num in points {
-        if num!=num {continue};
-        let num_sign:bool = num.signum() > 0.0;
-        sign = match sign {
-            None => {Some(num_sign)},
-            Some(old_sign) => {if old_sign != num_sign { return true } else {continue}}
-        };
-    }
-    false
-}
-
-fn apply_on_lattice<F> (pixel:Pixel, func: F, dim: u8) -> Vec<f64> where
+fn sign_change_on_lattice<F> (pixel:Pixel, func: F, dim: u8) -> bool where
     F: Fn(f64, f64) -> f64
 {
-    let mut output = Vec::with_capacity((dim*dim) as usize);
+    let mut sign: Option<bool> = None;
     for i in 0..dim {
         for j in 0..dim{
             let x = pixel.x+pixel.dx/((dim - 1) as f64)*(i as f64);
             let y = pixel.y+pixel.dy/((dim - 1) as f64)*(j as f64);
-            // println!("pos: {}, {}",x, y);
-            output.push(func(x,y));
+            let res = func(x,y);
+            let num_sign:bool = res.signum() > 0.0;
+            sign = match sign {
+                None => {Some(num_sign)},
+                Some(old_sign) if old_sign != num_sign => { return true },
+                _ => {continue}
+            };
         }
     }
-    // println!("values: {:?}",output);
-    output
-}
-
-fn within<F>(pixel:Pixel, eq: F) -> bool where
-    F: Fn(f64, f64) -> f64
-{
-    let x = pixel.x;
-    let y = pixel.y;
-    let dx = pixel.dx;
-    let dy = pixel.dy;
-    let v1 = eq(x + dx/2.0, y + dy/2.0);
-    if v1.abs() < ((dx/2.0).abs()).min((dy/2.0).abs()){
-        return true;
-    }
-    // if x.abs() < 0.01{
-        let points = apply_on_lattice(pixel, eq, 10);
-        sign_change(points.iter())
-    // }
-    // else{
-    //     let corners: [f64;4] = [eq(x, y), eq(x+dx, y), eq(x, y+dy), eq(x+dx, y+dy)];
-    //     sign_change(corners.iter())
-    // }
+    false
 }
 
 fn main() {
     let mut turtle = Turtle::new();
     // let eq = |x:f64, y:f64| x.sin() - y;
     let eq = |x:f64, y:f64| (1.0/x).sin() - y;
+    //let eq = |x:f64, y:f64| x*x + y*y - 3.0;
     turtle.drawing_mut().set_size((DRAW_X+50, DRAW_Y+50));
+    turtle.hide();
+    turtle.set_pen_size(1.0);
     turtle.set_speed("instant");
-    for i in -(DRAW_X as i32)/2..(DRAW_X as i32)/2 {
-        turtle.right(360.0/(DRAW_X as f64));
-        for j in -(DRAW_Y as i32)/2..(DRAW_Y as i32)/2 {
-            let pixel = conv(i, j);
-            if within(pixel, eq){
-                turtle.point(i as f64, j as f64);
+    for lattice_size in &[2u8, 3u8, 7u8, 13u8, 23u8, 87u8]{
+        turtle.drawing_mut().set_title(&format!("lattice {}x{}",&lattice_size, &lattice_size));
+        for i in -(DRAW_X as i32)/2..(DRAW_X as i32)/2 {
+            for j in -(DRAW_Y as i32)/2..(DRAW_Y as i32)/2 {
+                let pixel = conv(i, j);
+                if sign_change_on_lattice(pixel, eq, *lattice_size){
+                    turtle.point(i as f64, j as f64);
+                }
             }
         }
-    }
-}
-#[cfg(test)]
-mod tests{
-    use super::sign_change;
-    #[test]
-    fn test_sign_change(){
-        let data = [2.0, 2.0, 2.0, 2.0];
-        assert_eq!(sign_change(data.iter()), false);
-    }
-    #[test]
-    fn test_float_minmax_diff(){
-        let data = [-2.0, 2.0, 4.0, 2.0];
-        assert_eq!(sign_change(data.iter()), true);
     }
 }
