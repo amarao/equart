0923c5c369224d696b2b5125728e5f20ad8d5f52
George Shuklin <george.shuklin@gmail.com>
2019-05-01 22:50:36 +0300

    Yet another sin/cos play, mild work on imgref.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate turtle;
 extern crate imgref;
 
 use turtle::Turtle;
+use imgref::ImgVec;
 
 trait Point {
     fn point(&mut self, x:f64, y:f64);
@@ -20,7 +21,7 @@ impl Point for Turtle {
 
 
 struct Canvas {
-    canvas: Vec<u8>,
+    img: ImgVec<u32>,
     pix_dim_x: usize,
     pix_dim_y: usize,
     real_size_x: f64,
@@ -32,8 +33,8 @@ struct Canvas {
 
 impl Canvas{
     fn new(pix_dim_x: usize, pix_dim_y: usize, real_size_x: f64, real_size_y: f64, lattice_dim: usize) -> Canvas {
-        let canv = Vec::with_capacity(pix_dim_x * pix_dim_y);
-        Canvas{canvas: canv, pix_dim_x: pix_dim_x, pix_dim_y: pix_dim_y, real_size_x: real_size_x, real_size_y: real_size_y, lattice_dim:lattice_dim}
+        let img: ImgVec<u32> = ImgVec::new(vec![pix_dim_x as u32 * pix_dim_y as u32;0], pix_dim_x, pix_dim_y);
+        Canvas{img: img, pix_dim_x: pix_dim_x, pix_dim_y: pix_dim_y, real_size_x: real_size_x, real_size_y: real_size_y, lattice_dim:lattice_dim}
     }
     // fn dot(&self, i:usize, j:usize){
     //
@@ -42,11 +43,6 @@ impl Canvas{
 
 
 
-const DRAW_X: u32 = 1920;
-const DRAW_Y: u32 = 1080;
-const ZOOM: f64 = 20.0;
-const FUNC_X: f64 = 1.92*ZOOM;
-const FUNC_Y: f64 = 1.08*ZOOM/64.0;
 
 
 struct Pixel{x:f64, y:f64, dx:f64, dy:f64}
@@ -80,21 +76,31 @@ fn sign_change_on_lattice<F> (pixel:Pixel, func: F, dim: u8) -> bool where
     false
 }
 
+const DRAW_X: u32 = 1920;
+const DRAW_Y: u32 = 1080;
+const PAN: f64 = 150.0;
+const FUNC_X: f64 = 1.92*PAN/5.0;
+const FUNC_Y: f64 = 1.08*PAN;
+
+
 fn main() {
     let mut turtle = Turtle::new();
     //let eq = |x:f64, y:f64| x.sin() - y;
     // let eq = |x:f64, y:f64| (1.0/x).sin() - y;
     //let eq = |x:f64, y:f64| x*x + y*y - 3.0;
     // let eq = |x:f64, y:f64| x.sin() - y.cos() + 2.0*x.sin()*y.sin();
-    // let eq = |x:f64, y:f64| x.sin() - y.cos() - 2.0*x.sin()/y.sin();
+    //let eq = |x:f64, y:f64| x.sin() - y.cos() - 2.0*x.sin()/y.sin();
+    // let eq = |x:f64, y:f64| x.sin() - y.cos() - 2.0*x.sin()/y.sin() + (2.0*x).cos();
     // let eq = |x:f64, y:f64| (x*x).sin() - (y*y).cos();
     // let eq = |x:f64, y:f64| (x/y).sin();
-    let eq = |x:f64, y:f64| (1.0/x+1.0/y).sin()+x.sin();
+    //let eq = |x:f64, y:f64| (1.0/x+1.0/y).sin()+x.sin();
+    //let eq = |x:f64, y:f64| x.sin()-y.cos()-2.0*(x.sin())/(y.sin())+(2.0cos(2x)
+    let eq = |x:f64, y:f64| x.sin()- 3.0*x.cos()*(y/100.0).sin() + y.sin() - (3.0*y).cos()*(x/100.0).sin();
     turtle.drawing_mut().set_size((DRAW_X, DRAW_Y));
     turtle.hide();
     turtle.set_pen_size(1.0);
     turtle.set_speed("instant");
-    for lattice_size in &[63u8]{
+    for lattice_size in &[4u8]{
         turtle.drawing_mut().set_title(&format!("lattice {}x{}",&lattice_size, &lattice_size));
         for i in -(DRAW_X as i32)/2..(DRAW_X as i32)/2 {
             for j in -(DRAW_Y as i32)/2..(DRAW_Y as i32)/2 {
