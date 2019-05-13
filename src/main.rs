a29eac33cbdc63e0559ea59b8faa259cba9dd65a
George Shuklin <george.shuklin@gmail.com>
2019-05-13 08:22:57 +0300

    Typo

--- src/main.rs
+++ src/main.rs
@@ -288,6 +288,10 @@ fn main() {
     // let picture = (|x:Float, y:Float| (x*x+y*y)*sin(x*y)-PI, 1.92*470.0, 1.08*470.0, "sea of solicitude");
     // let picture = (|x:Float, y:Float| sin(x*cos(y))-cos(y*sin(x)), 1.92*60.0, 1.08*60.0, "tarnished lace");
     let picture = (|x:Float, y:Float| sin(x/y)-cos(y/x)+x-y, 1.92*2.8, 1.08*2.8, "trimed knot");
+    let mut canvas = Canvas::new(
+        1920,1080,
+        picture.1, picture.2,
+        1920/2, 1080/2
     );
     let now = Instant::now();
     render(&mut canvas, &picture.0, 2);
