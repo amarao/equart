1d86d26f1f2a3f94d8ab8bf40dd5ceabc4ea4e44
George Shuklin <george.shuklin@gmail.com>
2020-11-22 13:42:16 +0200

    Some buggy code to calculate roots, etc.

--- src/main.rs
+++ src/main.rs
@@ -183,35 +183,84 @@ fn equart(x: f64, y:f64) -> f64{
     x.sin() - y
 }
 
-struct Equart {
+struct Equart {  // per thread instance, each instance has own 'slice' to work with
     root_window_start_x: f64,
-    root_window_start_y: f64,
     root_window_end_x: f64,
+    root_window_start_y: f64,
     root_window_end_y: f64,
     roots_map: Vec<Vec<f64>>,
-    fixel_size: f64, //pixel size for float window
+    fixel_size_x: f64, //pixel size for float window
+    fixel_size_y: f64, //pixel size for float window
+    pixel_size_x: u32,
+    pixel_size_y: u32
 }
 
 impl DrawingApp for Equart{
     fn new(id: usize, max_id: usize, x: u32, y: u32)->Self {
-        Self{
-            root_window_start_x: 0.0,
-            root_window_start_y: 0.0,
-            root_window_end_x: 1.0,
-            root_window_end_y: 1.0,
+        let slice = Equart::slice(WINDOW_Y_START, WINDOW_Y_END, id, max_id);
+        let mut value = Self{
+            root_window_start_x: WINDOW_X_START,
+            root_window_end_x: WINDOW_X_END,
+            root_window_start_y: slice.0,
+            root_window_end_y: slice.1,
             roots_map: vec![vec![1.0, 2.0], vec![3.0, 4.0]],
-            fixel_size: 0.1
-        }
+            fixel_size_x: 1.0,
+            fixel_size_y: 1.0,
+            pixel_size_x: x,
+            pixel_size_y: y
+
+        };
+        value.update_fixel_size();
+        value
+        
     }
     fn resize(&mut self, old_x: u32, old_y: u32, new_x: u32, new_y: u32){
-
+        self.pixel_size_x = new_x;
+        self.pixel_size_y = new_y;
+        self.update_fixel_size();
     }
-    fn calculate_pixel(&mut self, _x: u32, _y: u32) -> im::Rgba<u8> {
-        im::Rgba([
-            0,
-            1,
-            2,
-            255
-        ])
+
+    fn calculate_pixel(&mut self, x: u32, y: u32) -> im::Rgba<u8> {
+        let matrix = self.pixel2matrix(x, y);
+        match Equart::is_root(matrix) {
+            true => im::Rgba([0, 0, 0, 255]),
+            false => im::Rgba([255 ,255, 255, 255]),
+        }
+    }
+}
+
+impl Equart{
+    fn is_root(matrix: Vec<[f64;2]>) -> bool {
+        let mut zeroes: bool = false;
+        let mut positive: bool = false;
+        let mut negative: bool = false;
+        for [x,y ] in matrix {
+            match equart(x, y).signum() {
+                0.0 => {zeroes = true;},
+                -1.0 => {negative = true;},
+                1.0 => {positive = true},
+                _ => {} // NaN
+            }
+        }
+        zeroes || (positive && negative)
+    }
+
+    fn pixel2matrix(&self, x: u32, y: u32)-> Vec<[f64;2]> {
+        // only 2x2 fixel matrix
+        let start_x = self.root_window_start_x + self.fixel_size_x * x as f64;
+        let start_y = self.root_window_start_y + self.fixel_size_y * y as f64;
+        let mut retval: Vec<[f64;2]> = Vec::with_capacity(4);
+        retval[0] = [start_x, start_y];
+        retval[1] = [start_x + self.fixel_size_x, start_y];
+        retval[2] = [start_x, start_y + self.fixel_size_y];
+        retval[3] = [start_x + self.fixel_size_x, start_y + self.fixel_size_y];
+        retval
+    }
+    fn slice(start: f64, end: f64, id: usize, max_id: usize) -> (f64, f64){
+        (0.0, 1.0)
+    }
+    fn update_fixel_size(&mut self){
+        self.fixel_size_x = (self.root_window_end_x - self.root_window_start_x)/self.pixel_size_x as f64;
+        self.fixel_size_y = (self.root_window_end_y - self.root_window_start_y)/self.pixel_size_y as f64;
     }
 }
