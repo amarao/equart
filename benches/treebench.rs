ffb8bb199795dd0f3985c227d665d14adea48699
George Shuklin <george.shuklin@gmail.com>
2021-01-15 22:12:32 +0200

    Updated benchmarks for deep/wide search

--- benches/treebench.rs
+++ benches/treebench.rs
@@ -1,57 +1,65 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use equart::quadtree::*;
 
-pub fn bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("quadtree");
-    group.bench_function("fill_20x20", |b| b.iter(
+pub fn fill_deep(c: &mut Criterion) {
+    c.bench_function("fill_deep", |b| b.iter(
         ||{
             let mut q = QuadTree::new(Boundry::from_coords(0.0, 0.0, 20.0, 20.0));
-            for x in 0..20{
-                for y in 0..20{
+            for x in 1..201{
+                for y in 1..201{
+                    let p = Point::new(black_box(1.0/x as f64), black_box(1.0/y as f64));
+                    black_box(q.append_point(p, black_box((x,y))));
+                }
+            }
+        }
+    ));
+}
+
+pub fn fill_wide(c: &mut Criterion) {
+    c.bench_function("fill_wide", |b| b.iter(
+        ||{
+            let mut q = QuadTree::new(Boundry::from_coords(0.0, 0.0, 200.0, 200.0));
+            for x in 0..200{
+                for y in 0..200{
                     let p = Point::new(black_box(x as f64), black_box(y as f64));
                     black_box(q.append_point(p, black_box((x,y))));
                 }
             }
         }
     ));
-    let mut q = QuadTree::new(Boundry::from_coords(0.0, 0.0, 20.0, 20.0));
-    for x in 0..20{
-        for y in 0..20{
+}
+
+pub fn search_deep(c: &mut Criterion) {
+    let mut q = QuadTree::new(Boundry::from_coords(0.0, 0.0, 200.0, 200.0));
+    for x in 1..201{
+        for y in 1..201{
+            let p = Point::new(black_box(1.0/x as f64), black_box(1.0/y as f64));
+            black_box(q.append_point(p, black_box((x,y))));
+        }
+    }
+
+    c.bench_function("search_deep", |b| b.iter(
+        ||{
+            q.values_in_area(Boundry::from_coords(0.0, 0.0, 1.0, 1.0));
+        }
+    ));
+}
+
+pub fn search_wide(c: &mut Criterion) {
+    let mut q = QuadTree::new(Boundry::from_coords(0.0, 0.0, 200.0, 200.0));
+    for x in 0..200{
+        for y in 0..200{
             let p = Point::new(black_box(x as f64), black_box(y as f64));
             black_box(q.append_point(p, black_box((x,y))));
         }
     }
-    group.bench_function("search_positive_20x20", |b| b.iter(
-        
-        
+    c.bench_function("search wide", |b| b.iter(
         ||{
-            
-            for x in 0..20{
-                for y in 0..20{
-                    black_box(q.search(black_box(Point::new(x as f64, y as f64))));
-                }
-            }
-        }
-    ));
-    group.bench_function("search_negative_20x20", |b| b.iter(
-        ||{
-            for x in 0..20{
-                for y in 0..20{
-                    black_box(q.search(Point::new(0.9999 * x as f64, 0.9999 *y as f64)));
-                }
-            }
+            q.values_in_area(Boundry::from_coords(50.0, 50.0, 150.0, 150.0));
         }
     ));
 }
 
-
-pub fn bench_subarea(c: &mut Criterion) {
-    c.bench_function("find_subarea", |b| b.iter(||{
-        let b = Boundry::from_coords(0.0, 0.0, 1.0, 1.0);
-        b.find_subarea(black_box(Point::new(0.5, 0.5)));
-    }));
-}
-
-criterion_group!(bench1, bench);
-criterion_group!(bench2, bench_subarea);
-criterion_main!(bench1, bench2);
+criterion_group!(bench_fill, fill_deep, fill_wide);
+criterion_group!(bench_search, search_deep, search_wide);
+criterion_main!(bench_fill, bench_search);
