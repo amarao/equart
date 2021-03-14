7de077eaaff13329f7d54a408a562f5b86af45bc
George Shuklin <george.shuklin@gmail.com>
2021-03-14 22:43:13 +0200

    Initial work on Screen (no multithreading).

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,50 @@
+use std::sync::atomic::{AtomicU32};
+use std::sync::atomic::Ordering::Relaxed;
+pub struct Screen{
+    pixels: Vec<std::sync::atomic::AtomicU32>,
+    width: u32,
+    height: u32
+}
 
+const BYTES_PER_PIXEL: usize = 4;
+impl Screen{
+    pub fn new(width: u32, height: u32) -> Result<Self, &'static str> {
+        let size = (width  * height) as usize;
+        if size % 8 !=0 {
+            return Err("Width * height should be multiplier of 2.")
+        }
+        let mut draw_buffer = Screen{
+            pixels: Vec::with_capacity(size),
+            width,
+            height
+        };
+        draw_buffer.pixels.resize_with(size, || AtomicU32::new(0xFFFFFFFF));
+        Ok(draw_buffer)
+    }
+    
+    /// Copy Screen content to u8 buffer
+    /// 
+    /// Buffer must be the same size as Screen.
+    pub fn copy_to_buffer<T>(&self, target: &mut [T]) -> Result<(), &'static str>
+    where T: bytemuck::Pod{
+        if self.pixels.len() * BYTES_PER_PIXEL != target.len() * std::mem::size_of_val(&target[0]){
+            return Err("Size of target should be the same as source.")
+        }
+        let target_u32: &mut [u32] = bytemuck::cast_slice_mut(target);
+        for idx in 0..self.pixels.len(){
+            target_u32[idx] = self.pixels[idx].load(Relaxed);
+        }
+        Ok(())
+    }
+
+    /// Fill whole Screen with specific color
+    pub fn fill(&mut self, color: u32) {
+        for b in &mut self.pixels{
+            b.store(color, Relaxed);
+        }
+    }
+
+    pub fn test(&self) -> u32{
+        self.pixels[0].load(Relaxed)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use lib::*;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("rust-sdl2 noise demo", 0, 0)
+        .window("equart", 0, 0)
         .fullscreen_desktop()
         .borderless()
         .build()
@@ -19,6 +20,7 @@ fn main() {
         .unwrap();
     sdl_context.mouse().show_cursor(false);
     let (width, height) = canvas.output_size().unwrap();
+    let mut screen = Screen::new(width, height).unwrap();
     let texture_creator = canvas.texture_creator();
     let mut whole_screen = texture_creator
         .create_texture_streaming(
@@ -27,17 +29,26 @@ fn main() {
             height as u32,
         )
         .unwrap();
-    whole_screen
-        .update(
-            //fill?
-            None,
-            &vec![0; (width * height * 4) as usize],
-            (width * 4) as usize,
-        )
-        .unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
     whole_screen.set_blend_mode(sdl2::render::BlendMode::None);
+    let mut cnt = 0;
     'running: loop {
+        cnt += 1;
+
+            
+            screen.fill(cnt);
+            whole_screen.with_lock(
+                None,
+                |bytearray, _|{
+                    screen.copy_to_buffer(bytearray).unwrap();
+                    // for b in bytearray{
+                    //     *b = cnt as u8;
+                    // }
+                    // println!("cnt: {}, {}", cnt, screen.test());
+                }
+            ).unwrap();
+        canvas.copy(&whole_screen, None, None).unwrap();
+        canvas.present();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -48,7 +59,5 @@ fn main() {
                 _ => {}
             }
         }
-        canvas.copy(&whole_screen, None, None).unwrap();
-        canvas.present();
     }
 }
