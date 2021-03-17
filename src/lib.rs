977f6f834b3e10939c4aa01f7b9bb08dc18c49a7
George Shuklin <george.shuklin@gmail.com>
2021-03-17 16:34:09 +0200

    EasyScreen done

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
-use std::sync::atomic::AtomicU32;
-use std::sync::atomic::Ordering::Relaxed;
+use sdl2::render::Canvas;
+use std::sync::atomic::{AtomicU32, Ordering::Relaxed};
 use std::sync::Arc;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
 #[derive(Clone)]
 pub struct RelaxedBuffer {
     data:  Arc<[AtomicU32]>,
-    width: u32,
-    height: u32,
 }
 impl RelaxedBuffer{
-    pub fn new(width: u32, height:u32, init_value: u32) -> Self {
-        let size = width.checked_mul(height).unwrap() as usize;
+    pub fn new(size:usize, init_value: u32) -> Self {
         if size == 0{
             panic!("width and height should be > 0");
         }
@@ -18,16 +17,17 @@ impl RelaxedBuffer{
         vec.resize_with(size, || AtomicU32::new(init_value));
         RelaxedBuffer {
                 data: Arc::from(vec),
-                width,
-                height,
         }
     }
+
     pub fn get(&self, idx: usize) -> u32 {
         self.data[idx].load(Relaxed)
     }
+
     pub fn set(&self, idx: usize, val: u32) {
         self.data[idx].store(val, Relaxed);
     }
+
     pub fn fill(&self, val: u32){
         for idx in 0..self.data.len(){
             unsafe {
@@ -35,6 +35,7 @@ impl RelaxedBuffer{
             }
         }
     }
+
     pub fn copy_into_slice<T>(&self, dest: &mut [T])
     where T: bytemuck::Pod {
             let target_u32: &mut [u32] = bytemuck::cast_slice_mut(dest);
@@ -43,5 +44,84 @@ impl RelaxedBuffer{
                     *target_u32.get_unchecked_mut(idx) = self.data.get_unchecked(idx).load(Relaxed);
                 }
             }
+    }
+}
+
+pub struct EasyScreen{
+    buff: RelaxedBuffer,
+    width: u32,
+    height: u32,
+    sdl_thread: std::thread::JoinHandle<()>,
+}
+
+impl Default for EasyScreen {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl EasyScreen{
+    pub fn new() -> Self{
+        let (tx, rx) = std::sync::mpsc::sync_channel(1);
+        let sdl_thread = std::thread::spawn(move || {EasyScreen::display_thread(tx);});
+        let (buff, width, height) = rx.recv().unwrap();
+        EasyScreen{buff, width, height, sdl_thread}
+    }
+
+    fn display_thread(tx: std::sync::mpsc::SyncSender<(RelaxedBuffer, u32, u32)>){
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+        let window = video_subsystem
+            .window("title to be replace by a builder", 0, 0)
+            .fullscreen_desktop()
+            .borderless()
+            .build()
+            .unwrap();
+        let mut canvas = window
+            .into_canvas()
+            .present_vsync()
+            .accelerated()
+            .build()
+            .unwrap();
+        sdl_context.mouse().show_cursor(false);
+        let (width, height) = canvas.output_size().unwrap();
+        
+        let texture_creator = canvas.texture_creator();
+        let mut whole_screen = texture_creator
+            .create_texture_streaming(
+                sdl2::pixels::PixelFormatEnum::ABGR8888,
+                width as u32,
+                height as u32,
+            )
+            .unwrap();
+        let mut event_pump = sdl_context.event_pump().unwrap();
+        whole_screen.set_blend_mode(sdl2::render::BlendMode::None);
+
+        let buff = RelaxedBuffer::new((width * height) as usize, 0);
+        tx.send((buff.clone(), width, height)).unwrap();
+        loop {
+            whole_screen.with_lock(
+                None,
+                |bytearray, _|{
+                    buff.copy_into_slice(bytearray);
+                }
+            ).unwrap();
+            canvas.copy(&whole_screen, None, None).unwrap();
+            canvas.present();
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => std::process::exit(0),
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    pub fn fill(&self, color: u32){
+        self.buff.fill(color);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,11 @@
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use lib::RelaxedBuffer;
+use lib::EasyScreen;
 
-fn draw(buff: RelaxedBuffer){
+fn main(){
+    let screen = EasyScreen::new();
     let mut c = 0;
-    let mut start = std::time::Instant::now();
-    let  mut last_printed:u64 = 0;
-    let mut frames:u64 = 0;
     loop{
         c+=1;
-        frames += 1;
-        buff.fill(c);
-        if start.elapsed() > std::time::Duration::new(1, 0){
-            let dt = start.elapsed().as_secs_f64();
-            let fc = frames - last_printed;
-            last_printed = frames;
-            start = std::time::Instant::now();
-            println!("\ncalc fps: {:.1}\n", fc as f64/dt);
-        }
+        screen.fill(c);
     }
 }
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem
-        .window("equart", 0, 0)
-        .fullscreen_desktop()
-        .borderless()
-        .build()
-        .unwrap();
-    let mut canvas = window
-        .into_canvas()
-        .present_vsync()
-        .accelerated()
-        .build()
-        .unwrap();
-    sdl_context.mouse().show_cursor(false);
-    let (width, height) = canvas.output_size().unwrap();
-    let screen = RelaxedBuffer::new(width, height, 0);
-    let texture_creator = canvas.texture_creator();
-    let mut whole_screen = texture_creator
-        .create_texture_streaming(
-            sdl2::pixels::PixelFormatEnum::ABGR8888,
-            width as u32,
-            height as u32,
-        )
-        .unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    whole_screen.set_blend_mode(sdl2::render::BlendMode::None);
-    let second_screen = screen.clone();
-    let mut start = std::time::Instant::now();
-    let  mut last_printed:u64 = 0;
-    let mut frames:u64 = 0;
-    std::thread::spawn(move ||draw(second_screen));
-    loop {
-            frames +=1;
-            whole_screen.with_lock(
-                None,
-                |bytearray, _|{
-                    screen.copy_into_slice(bytearray);
-                }
-            ).unwrap();
-        canvas.copy(&whole_screen, None, None).unwrap();
-        canvas.present();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => std::process::exit(0),
-                _ => {}
-            }
-        }
-        if start.elapsed() > std::time::Duration::new(1, 0){
-            let dt = start.elapsed().as_secs_f64();
-            let fc = frames - last_printed;
-            last_printed = frames;
-            start = std::time::Instant::now();
-            println!("\ndraw fps: {:.1}\n", fc as f64/dt);
-        }
-    }
-}
