5b3fc7b361ddf89ee9461507ba988f58685ba609
George Shuklin <george.shuklin@gmail.com>
2021-03-20 14:17:56 +0200

    WIP on gen_position

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use lib::EasyScreen;
 mod quadtree;
 pub use crate::quadtree::{QuadTree, Point};
-use std::num::FpCategory::*;
+use std::num::FpCategory;
 
 #[derive (PartialEq)]
 enum FixelState{
@@ -14,6 +14,8 @@ enum FixelState{
 
 struct Fixel{
     center: Point,
+    dx: f64,
+    dy: f64,
     probes: Vec<(Point, f64)>,
     state: FixelState,
     max: Option<f64>,
@@ -21,9 +23,11 @@ struct Fixel{
 }
 
 impl Fixel{
-    fn new(center: Point) -> Self{
+    fn new(center: Point, dx: f64, dy: f64) -> Self{
         Fixel{
             center,
+            dx,
+            dy,
             probes: Vec::with_capacity(4),
             state: FixelState::NoProbes,
             max: None,
@@ -45,33 +49,52 @@ impl Fixel{
         }
         for probe in self.probes.iter(){
             match probe.1.classify(){
-                Nan | Infinite => {
+                FpCategory::Nan | FpCategory::Infinite => {
                     self.state = FixelState::NoDomain;
                     return;
                 },
-                Zero => {
+                FpCategory::Zero => {
                     self.state = FixelState::ExactRoot;
                     return;
                 },
                 _ => {}
             }
-            
+            self.max = Some(self.max.map_or(probe.1, |v| v.max(probe.1)));
+            self.min = Some(self.min.map_or(probe.1, |v| v.min(probe.1)));
+        }
+        if self.max.is_some() && self.min.is_some() && self.max.unwrap() > 0.0 && self.min.unwrap() < 0.0 {
+            self.state = FixelState::SignChangeRoot;
         }
     }
     fn gen_positions(&self, samples: usize) -> Vec<Point>{
+        let mut positions = Vec::with_capacity(samples);
+        let side = (samples as f64).sqrt().ceil() as usize;
+        let x_step = self.dx/side as f64;
+        let y_step = self.dy/side as f64;
+        let start_x = self.center.x - self.dx/2.0;
+        let start_y = self.center.y - self.dy/2.0;
+        for x_cnt in 0..side{
+            for y_cnt in 0..side{
+                let point = Point::new(
+                    start_x + x_step * x_cnt as f64,
+                    start_y + y_step * y_cnt as f64,
+                );
+                positions.push(point);
+            }
+        }
         vec![Point::new(0.0, 0.0)]
     }
 
+    /// Assure there is a least a 'samples' number of probes, if they are
+    /// needed.
     fn do_probes<F>(&mut self, samples: usize, f: F)
     where F: Fn(Point) -> f64{
         if self.probes.len() >= samples || self.state == FixelState::ExactRoot || self.state == FixelState::SignChangeRoot{
             return 
         }
-        let mut new_points = false;
         for point in self.gen_positions(samples){
             if !self.already_present(point){
                 self.probes.push((point, f(point)));
-                new_points = true;
             }
         }
         self.update_cache();
@@ -79,7 +102,7 @@ impl Fixel{
 }
 
 fn equation(x: f64, y:f64) -> f64{
-    ((((x.sin()-y).cos()-x).sin()-y).cos()/(x*y).sin()/(x.abs().ln()*(y*y).sin())).ln()
+    x.sin()*(y*3.0).sin()-x.sin()-y.sin()
 }
 
 fn is_root(x: f64, y: f64, dx:f64, dy:f64) -> bool{
